fb0d5908fda06cbd8a6e552d5ab9c675427f857d
Oliver Kozub <Oliver.kozub@Icloud.com>
2023-12-24 20:21:42 -0600

    added logging and using mins like a normal person

--- main.py
+++ main.py
@@ -12,24 +12,37 @@ def show_image(image_path):
     panel = tkinter.Label(window, image=img)
     panel.pack(side="bottom", fill="both", expand="yes")
 
-    time.sleep(random.randint(1, 15) * 1000)
+    random_time = random.randint(1, 15) 
+
+    print("Waiting for", random_time, "seconds")
+
+    time.sleep(random_time)
+
+    print("Destorying Window")
 
     window.destroy()
 
 def get_random_image():
     image_folder_path = os.path.join(os.getcwd(), "img")
     image_files = [f for f in os.listdir(image_folder_path) if os.path.isfile(os.path.join(image_folder_path, f))]
-    image_path = os.path.join(image_folder_path, random.choice(image_files))
+    image_random_selected = random.choice(image_files)
+    image_path = os.path.join(image_folder_path, image_random_selected)
 
-    print("Image Path Found", image_path)
+    print("Image Selected", image_random_selected)
 
     return image_path
 
 def init():
     show_image(get_random_image())
     
-    time.sleep(random.randint(60, 120) * 1000)
+    random_sleep_amount = random.randint(60, 120)
+
+    print("Waiting for", random_sleep_amount, "Seconds")
     
+    time.sleep(random_sleep_amount)
+    
+    print("Restarying Cycle")
+
     init()
 
 init()
